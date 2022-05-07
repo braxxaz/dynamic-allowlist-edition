408d1e56e5a1ff60b13848bf5b0dc30d5b22c203
braxxaz <braxxaz@gmail.com>
2022-05-07 12:42:31 -0600

    INIT 66

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -4,7 +4,7 @@ import React from "react"
 const Main = () => {
   return (
     <div className={'video'}>
-      <video src={"https://website-braxxaz.s3.us-east-2.amazonaws.com/nfts+intro+web.mp4"} autoPlay playsinlineloop muted type='video/mp4' />
+      <video src={"https://website-braxxaz.s3.us-east-2.amazonaws.com/nfts+intro+web.mp4"} autoPlay="true" loop preload="auto" playsinlineloop muted="true" type='video/mp4' />
     </div>
   )
 }
