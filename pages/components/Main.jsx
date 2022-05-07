acc63ec69ff9f7c97b5dab9d91223b98ad137034
braxxaz <braxxaz@gmail.com>
2022-05-06 23:18:51 -0600

    Init 66

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -4,7 +4,7 @@ import React from "react"
 const Main = () => {
   return (
 <div dangerouslySetInnerHTML={{ __html: `
-  <video src={"https://website-braxxaz.s3.us-east-2.amazonaws.com/nfts+intro+web.mp4%22%7D autoPlay loop muted type='video/mp4' />
+  <video src={"https://website-braxxaz.s3.us-east-2.amazonaws.com/nfts+intro+web.mp4%22%7D autoPlay playsinline loop muted type='video/mp4' class="{'video'}" />
 ` }}></div>
   )
 }
